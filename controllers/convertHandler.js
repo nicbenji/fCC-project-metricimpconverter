0ed923d99fc4e1e798366fcb00ca78e5cff3bd0d
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-01 21:02:35 +0100

    getNum implemented using TDD

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,60 @@
+
 function ConvertHandler() {
-  
+
   this.getNum = function(input) {
-    let result;
-    
-    return result;
+    const number = input.match(/(\d*\.?\d+)/g);
+    console.log(number);
+    if (n === null) {
+      return 1;
+    }
+    if (number.length > 2) {
+      throw new Error('invalid number');
+    }
+
+    // Handle fractionals
+    if (input.includes('/')) {
+      const numerator = number[0];
+      const denominator = number[1];
+      return numerator / denominator;
+    } else {
+      return Number(number[0]);
+    }
   };
-  
+
+
   this.getUnit = function(input) {
     let result;
-    
+
     return result;
   };
-  
+
   this.getReturnUnit = function(initUnit) {
     let result;
-    
+
     return result;
   };
 
   this.spellOutUnit = function(unit) {
     let result;
-    
+
     return result;
   };
-  
+
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
     let result;
-    
+
     return result;
   };
-  
+
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     let result;
-    
+
     return result;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,6 +4,42 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 let convertHandler = new ConvertHandler();
 
-suite('Unit Tests', function(){
+suite('Unit Tests', function() {
+
+  suite('getNum()', () => {
+
+    test('should correctly read a whole number from input', () => {
+      const input = '345km';
+      assert.deepEqual(convertHandler.getNum(input), 345);
+    });
+
+    test('should correctly read a decimal number from input', () => {
+      const input = '35.956lbs';
+      assert.deepEqual(convertHandler.getNum(input), 35.956);
+    });
+
+    test('should correctly read a fractional from input', () => {
+      const input = '2/5';
+      assert.deepEqual(convertHandler.getNum(input), 0.4);
+    });
+
+    test('should correctly read a fractional with decimals from inputs', () => {
+      let input = '2.34/4';
+      assert.deepEqual(convertHandler.getNum(input), 0.585);
+      input = '31/7.82';
+      assert.deepEqual(convertHandler.getNum(input), 3.9641943734015346);
+    });
+
+    test('should incorrectly return an error on a double-fraction', () => {
+      const input = '3/2/3';
+      assert.throw(() => convertHandler.getNum(input), 'invalid number');
+    });
+
+    test('should use a value of 1 if only unit is provided', () => {
+      const input = 'gal';
+      assert.deepEqual(convertHandler.getNum(input), 1);
+    })
+
+  });
 
 });
