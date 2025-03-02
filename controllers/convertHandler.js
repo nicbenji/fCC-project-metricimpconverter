98564d5bcb6ea84156f0ee3c7d3af3d103096048
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-02 23:36:34 +0100

    implemented getString()

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -108,9 +108,7 @@ function ConvertHandler() {
   };
 
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-
-    return result;
+    return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
 
 }
