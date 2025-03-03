502d721f794f112ba4624c03706bdc7d8c4b4dd7
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-03 23:26:41 +0100

    added endpoint and functional tests -> regex are making problems

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,48 @@ module.exports = function(app) {
   let convertHandler = new ConvertHandler();
 
   app.route('/api/convert').get((req, res) => {
+    const input = req.query.input;
+    try {
+      const { initNum, initUnit } = handleBadRequest(input);
+      const returnNum = convertHandler.convert(initNum, initUnit);
+      const returnUnit = convertHandler.getReturnUnit(initUnit);
+      const resString = convertHandler
+        .getString(initNum, initUnit, returnNum, returnUnit);
+      res.json({ initNum, initUnit, returnNum, returnUnit, string: resString });
+    } catch (error) {
+      console.error(error.message);
+      res.send(error.message);
+    }
   });
 
+  function handleBadRequest(input) {
+
+    let numError, unitError;
+    let initNum, initUnit;
+
+    try {
+      initNum = convertHandler.getNum(input);
+    } catch (error) {
+      numError = error.message;
+    }
+
+    try {
+      initUnit = convertHandler.getUnit(input);
+    } catch (error) {
+      unitError = error.message;
+    }
+
+    if (numError && unitError) {
+      throw new Error('invalid number and unit');
+    } else if (numError) {
+      throw new Error(numError);
+    } else if (unitError) {
+      throw new Error(unitError);
+    }
+
+    return { initNum, initUnit };
+
+  }
 };
+
+
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -9,19 +9,63 @@ suite('Functional Tests', function() {
 
   suite('/api/convert endpoint', () => {
 
-    test('should convert a valid input', () => {
+    test('should convert a valid input', (done) => {
+      chai.request(server)
+        .get('/api/convert?input=10L')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, '{"initNum":10,"initUnit":"L",'
+            + '"returnNum":2.6417217685798895,"returnUnit":"gal",'
+            + '"string":"10 liters converts to 2.6417217685798895 gallons"}');
+
+          done();
+        });
     });
 
-    test('should return the correct error message on invalid unit', () => {
+    test('should return the correct error message on invalid unit', (done) => {
+      chai.request(server)
+        .get('/api/convert?input=32g')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'invalid unit');
+
+          done();
+        });
     });
 
-    test('should return the correct error message on invalid number', () => {
+    test('should return the correct error message on invalid number', (done) => {
+      chai.request(server)
+        .get('/api/convert?input=3/7.2/4kg')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'invalid number');
+
+          done();
+        });
     });
 
-    test('should return the correct error message on invalid number and unit', () => {
+    test('should return the correct error message on invalid number and unit', (done) => {
+      chai.request(server)
+        .get('/api/convert?input=3/7.2/4kilomegagram')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'invalid number and unit');
+
+          done();
+        });
     });
 
-    test('should convert an input with just a correct unit', () => {
+    test('should convert an input with just a correct unit', (done) => {
+      chai.request(server)
+        .get('/api/convert?input=kg')
+        .end((err, res) => {
+          assert.equal(res.status, 200);
+          assert.equal(res.text, '{"initNum":1,"initUnit":"kg",'
+            + '"returnNum":2.20462,"returnUnit":"lbs",'
+            + '"string":"1 kilograms converts to 2.20462 pounds"}');
+
+          done();
+        });
     });
 
   });
