3e7dcbf2836d20e386d2e9e381e56df8ce605e9e
nicbenji <172269214+nicbenji@users.noreply.github.com>
2025-03-03 19:16:52 +0100

    added empty route and tests

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,11 @@
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
-  
+module.exports = function(app) {
+
   let convertHandler = new ConvertHandler();
+
+  app.route('/api/convert').get((req, res) => {
+  });
 
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,4 +7,23 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
+  suite('/api/convert endpoint', () => {
+
+    test('should convert a valid input', () => {
+    });
+
+    test('should return the correct error message on invalid unit', () => {
+    });
+
+    test('should return the correct error message on invalid number', () => {
+    });
+
+    test('should return the correct error message on invalid number and unit', () => {
+    });
+
+    test('should convert an input with just a correct unit', () => {
+    });
+
+  });
+
 });
